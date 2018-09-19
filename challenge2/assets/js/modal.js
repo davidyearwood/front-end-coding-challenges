274bf2bde0a20d10aa4b46ea78f675c154b79fa5
David <dwhywood@gmail.com>
2018-09-19 16:10:37 -0500

    Add special note text under rates and change container width

--- challenge2/assets/js/modal.js
+++ challenge2/assets/js/modal.js
@@ -4,6 +4,7 @@
 
     Array.prototype.forEach.call(toggleButtons, function(element) {
         element.addEventListener('click', function(e) {
+            e.preventDefault();
             var target = document.getElementById(e.target.dataset.target); 
             target.classList.toggle('is-active');
         });
