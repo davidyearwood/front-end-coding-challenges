03517d98d045b0ed637977230e97b6f1fd2076f5
David <dwhywood@gmail.com>
2018-10-08 13:55:16 -0500

    Remove old polyfill libraries from import

--- challenge2/src/index.js
+++ challenge2/src/index.js
@@ -1,6 +1,6 @@
+import 'whatwg-fetch'
 import './map-polyfill.js';
 import 'promise-polyfill/src/polyfill';
-import 'fetch-polyfill';
 import './populateDataTable.js';
 import './toggleTabs.js';
 import toggleModal from './toggleModal.js';
